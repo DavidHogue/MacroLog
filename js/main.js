4b194ccb94610262f7d49fb4d0e74a394464c522
David Hogue <david@davidhogue.com>
2013-05-27 11:24:22 -0700

    Added require.js, added ability to view by date and delete foods, rewrote most of test.js to make jslint happy.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+requirejs.config({
+    paths: {
+        "jquery": "http://code.jquery.com/jquery-1.10.0.min"
+    },
+    shim: {
+        "jquery": {
+            exports: "jQuery"
+        }
+    }
+});
+
+require(["test"]);
--- js/test.js
+++ js/test.js
@@ -1,6 +1,93 @@
-(function() {
+require(['jquery'], function($) {
 
-    var _database = "http://localhost:5984/macrolog";
+    "use strict";
+
+    var database = "http://localhost:5984/macrolog",
+        date;
+
+    function prettyDate(d) {
+        return d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+    }
+
+    function loadFoods() {
+        var deferred = new $.Deferred();
+
+        $.ajax(database + "/_design/foods/_view/date", {
+            type: "GET",
+            data: prettyDate(date)
+        }).done(function(data) {
+            var parsed = JSON.parse(data),
+                results = [],
+                i,
+                food;
+            for (i = 0; i < parsed.total_rows; i += 1) {
+                food = parsed.rows[i].value;
+                food.calories = food.calories || 0;
+                food.fat = food.fat || 0;
+                food.carb = food.carb || 0;
+                food.protein = food.protein || 0;
+                food.quantity = food.quantity || 1;
+                results.push(food);
+            }
+            deferred.resolve(results);
+        });
+
+        return deferred;
+    }
+
+    function deleteFood() {
+        var $button = $(this),
+            rev = $button.data('rev'),
+            id = $button.data('id');
+        $.ajax(database + "/" + id + "?rev=" + encodeURIComponent(rev), {
+            type: "DELETE"
+        }).done(function() {
+            $button.parent().remove();
+        });
+    }
+
+    function getFoodDiv(food) {
+        var $div = $("<div>"),
+            multiplier = food.quantity && food.quantity !== 1 ? " (x" + food.quantity + ")" : "",
+            button;
+        $div.append($("<h4>").text(food.name + multiplier + ":"));
+        $div.append($("<div>").text("Calories: " + food.calories));
+        $div.append($("<div>").text("Fat: " + food.fat));
+        $div.append($("<div>").text("Carbs: " + food.carb));
+        $div.append($("<div>").text("Protein: " + food.protein));
+        if (food.name !== "Totals") {
+            button = $('<a class="btn btn-danger" href="#"><i class="icon-trash icon-white"></i> Delete</a>')
+                            .data('id', food._id)
+                            .data('rev', food._rev)
+                            .click(deleteFood);
+            $div.append(button);
+        }
+        return $div;
+    }
+
+    function sumFood(totals, food) {
+        totals.calories += food.calories * food.quantity;
+        totals.fat += food.fat * food.quantity;
+        totals.carb += food.carb * food.quantity;
+        totals.protein += food.protein * food.quantity;
+    }
+
+    function showFoods() {
+        var $foods = $("#foods");
+        $foods.empty();
+
+        loadFoods().done(function(foods) {
+            var totals = { name: "Totals", calories: 0, fat: 0, carb: 0, protein: 0 },
+                i,
+                food;
+            for (i = 0; i < foods.length; i += 1) {
+                food = foods[i];
+                sumFood(totals, food);
+                $foods.append(getFoodDiv(food));
+            }
+            $foods.append(getFoodDiv(totals));
+        });
+    }
 
     function addFood() {
         // Get an object from the form data.
@@ -11,82 +98,31 @@
             carb: parseFloat($("#carb").val()),
             protein: parseFloat($("#protein").val()),
             quantity: parseFloat($("#quantity").val()),
+            date: prettyDate(date)
         };
-        
+
         // Write to db.
-        $.ajax(_database, {
+        $.ajax(database, {
             data: JSON.stringify(food),
             contentType: "application/json",
             type: "POST"
         }).done(function() {
             showFoods();
         });
-        
+
         // Prevent normal button events.
         return false;
     }
-    
-    function loadFoods() {
-        var deferred = new $.Deferred();
-        
-        $.ajax(_database + "/_design/foods/_view/all", {
-            type: "GET"
-        }).done(function(data) {
-            var parsed = JSON.parse(data);
-            var results = [];
-            for (var i = 0; i < parsed.total_rows; i++) {
-                var food = parsed.rows[i].value;
-                var newFood = {
-                    name: food.name,
-                    calories: food.calories || 0,
-                    fat: food.fat || 0,
-                    carb: food.carb || 0,
-                    protein: food.protein || 0,
-                    quantity: food.quantity || 1
-                };
-                results.push(newFood);
-            }
-            deferred.resolve(results);
-        });
-        
-        return deferred;
-    }
-    
-    function getFoodDiv(food) {
-        var $div = $("<div>");   
-        var multiplier = food.quantity && food.quantity != 1 ? " (x" + food.quantity + ")" : "";
-        $div.append($("<h4>").text(food.name + multiplier + ":"));
-        $div.append($("<div>").text("Calories: " + food.calories));
-        $div.append($("<div>").text("Fat: " + food.fat));
-        $div.append($("<div>").text("Carbs: " + food.carb));
-        $div.append($("<div>").text("Protein: " + food.protein));
-        return $div;
-    }
-    
-    function sumFood(totals, food) {
-        totals.calories += food.calories * food.quantity;
-        totals.fat += food.fat * food.quantity;
-        totals.carb += food.carb * food.quantity;
-        totals.protein += food.protein * food.quantity;
-    }
-    
-    function showFoods() {
-        var $foods = $("#foods");
-        $foods.empty();
-        
-        loadFoods().done(function(foods) {
-            var totals = { name: "Totals", calories: 0, fat: 0, carb: 0, protein: 0 };
-            for(var i in foods) {
-                var food = foods[i];
-                sumFood(totals, food);                
-                $foods.append(getFoodDiv(food));
-            }
-            $foods.append(getFoodDiv(totals));
-        });
+
+    function showDate() {
+        date = new Date();
+        $('#date').text(prettyDate(date));
     }
 
     $(function() {
-        $("#add").click(addFood);
+        showDate();
         showFoods();
+
+        $("#add").click(addFood);
     });
-})();
+});
